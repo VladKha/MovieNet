ced78b4465a4a0dbaa50a63a622f6a8187440379
vladkha <vladislavkhaitov@gmail.com>
2017-02-09 17:53:30 +0200

    Clean up settings

--- MovieNet/wsgi.py
+++ MovieNet/wsgi.py
@@ -12,7 +12,11 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+#
+from dj_static import MediaCling
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MovieNet.settings")
 
 application = get_wsgi_application()
+application = MediaCling(application)
 application = DjangoWhiteNoise(application)
